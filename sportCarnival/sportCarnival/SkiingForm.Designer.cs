7396e0715e7536a0e4be779be9c82b0343745240
eecheng <yucheng871011@gmail.com>
2018-12-13 20:52:46 +0800

    add colliding feature

--- sportCarnival/sportCarnival/SkiingForm.Designer.cs
+++ sportCarnival/sportCarnival/SkiingForm.Designer.cs
@@ -30,11 +30,45 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelQ = new System.Windows.Forms.Label();
+            this.mypanel = new System.Windows.Forms.Panel();
+            this.ansLabel = new System.Windows.Forms.Label();
+            this.mypanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelQ
+            // 
+            this.labelQ.AutoSize = true;
+            this.labelQ.BackColor = System.Drawing.Color.Gray;
+            this.labelQ.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQ.Location = new System.Drawing.Point(574, 13);
+            this.labelQ.Name = "labelQ";
+            this.labelQ.Size = new System.Drawing.Size(96, 43);
+            this.labelQ.TabIndex = 0;
+            this.labelQ.Text = "題目:";
+            // 
+            // mypanel
+            // 
+            this.mypanel.Controls.Add(this.ansLabel);
+            this.mypanel.Location = new System.Drawing.Point(-1, 1);
+            this.mypanel.Name = "mypanel";
+            this.mypanel.Size = new System.Drawing.Size(1400, 700);
+            this.mypanel.TabIndex = 1;
+            // 
+            // ansLabel
+            // 
+            this.ansLabel.AutoSize = true;
+            this.ansLabel.BackColor = System.Drawing.Color.Gray;
+            this.ansLabel.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ansLabel.Location = new System.Drawing.Point(811, 12);
+            this.ansLabel.Name = "ansLabel";
+            this.ansLabel.Size = new System.Drawing.Size(96, 43);
+            this.ansLabel.TabIndex = 0;
+            this.ansLabel.Text = "完成:";
             // 
             // SkiingForm
             // 
@@ -42,16 +76,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1382, 653);
+            this.Controls.Add(this.labelQ);
+            this.Controls.Add(this.mypanel);
             this.Name = "SkiingForm";
             this.Text = "SkiingForm";
             this.Load += new System.EventHandler(this.SkiingForm_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SkiingForm_KeyDown);
+            this.mypanel.ResumeLayout(false);
+            this.mypanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelQ;
+        private System.Windows.Forms.Panel mypanel;
+        private System.Windows.Forms.Label ansLabel;
     }
 }
--- sportCarnival/sportCarnival/SkiingForm.cs
+++ sportCarnival/sportCarnival/SkiingForm.cs
@@ -12,15 +12,33 @@ namespace sportCarnival
 {
     public partial class SkiingForm : Form
     {   
+        private List<string> question;
+        private List<Label> brick;
+
         public int mapX;
+        
         public PictureBox mole;
         public PictureBox backImg;
         public int moleX;
         public int moleY;
         public int lastmove;//0:A,1:S,2:D,3:W
+
+        private int brickSpeed;
+        private int counter;
+
+        private string currentQ;
+        private char[] currentCharArr;
+        private bool[] eachCharState;
+        private string currentA;
+        private bool finish;
+
         public SkiingForm()
         {
             InitializeComponent();
+
+            initQuestion();
+            brickSpeed = 5;
+
             mapX = 0;
             lastmove = 0;
 
@@ -49,23 +67,31 @@ namespace sportCarnival
             moleX = 50;
             moleY = 300;
 
-            this.Controls.Add(mole);
-            this.Controls.Add(backImg);
+            mypanel.Controls.Add(mole);
+            mypanel.Controls.Add(backImg);
             
 
         }
 
         private void SkiingForm_Load(object sender, EventArgs e)
         {
-           
-            timer1.Interval = 5;
 
-            
+            counter = 0;
+            timer1.Interval = 5;
+            brick = new List<Label>();
+
+            finish = true;
+
+
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Random myrand = new Random();
+
+
+
             backImg.Location = new Point(mapX, 0);
             mole.Location = new Point(moleX, moleY);
 
@@ -116,8 +142,82 @@ namespace sportCarnival
                 default:
                     break;
             }
-        }
 
+            //generate Question
+            if (finish)
+            {
+                currentQ = question[myrand.Next(0, question.Count)];
+                currentCharArr = currentQ.ToCharArray();
+                eachCharState = new bool[currentQ.Length];
+                for (int i = 0; i < eachCharState.Length; i++)
+                    eachCharState[i] = false;
+                labelQ.Text = "題目:" + currentQ;
+                ansLabel.Text = "完成:";
+                finish = false;
+            }
+
+
+
+            //generate brick
+            counter++;
+            if (counter > 50)
+            {
+                counter = 0;
+                geneBrick();
+            }
+            //update each brick's position
+            for(int i = 0; i < brick.Count; i++)
+            {
+                brick[i].Left -= brickSpeed;
+            }
+            //check whether collide
+            for(int i = 0; i < brick.Count; i++)
+            {
+                int centerY = (brick[i].Top+brick[i].Bottom)/ 2;
+                int centerX = brick[i].Left;
+                if (centerX < mole.Right && centerX > mole.Left && centerY < mole.Bottom && centerY > mole.Top)
+                {
+                    //collide
+                    char[] arr = brick[i].Text.ToArray();
+                    //MessageBox.Show(arr[0].ToString());
+                    if (currentQ.IndexOf(arr[0]) != -1)
+                    {
+                        //Question contain brick.text which is collided currently
+                        eachCharState[currentQ.IndexOf(arr[0])] = true;
+
+                        //update content in ansLabel
+                        ansLabel.Text = "完成: ";
+                        for(int j = 0; j < eachCharState.Length; j++)
+                        {
+                            if (eachCharState[j])
+                            {
+                                ansLabel.Text += currentCharArr[j];
+                            }
+                        }
+                    }
+                    mypanel.Controls.Remove(brick[i]);
+                    brick[i].Dispose();
+                    brick.Remove(brick[i]);
+                }
+            }
+            // check whether finish current Question
+            if (complete())
+            {
+                // ~~~~~~~later to do~~~~~~~~~~~~~ 
+                MessageBox.Show("FD");
+            }
+
+        }
+        // check whether finish current Question
+        private bool complete()
+        {
+            for (int k = 0; k < eachCharState.Length; k++)
+            {
+                if (!eachCharState[k])
+                    return false;
+            }
+            return true;
+        }
         private void SkiingForm_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode.ToString() == "A")
@@ -141,5 +241,40 @@ namespace sportCarnival
        
             }
         }
+
+        private void geneBrick()
+        {
+            Random myrand = new Random();
+            //Graphics g;
+            int num = myrand.Next(65, 70);
+            char ch = (char)num;
+            
+            
+            Label tmp = new Label();
+            tmp.Text = ch.ToString();
+            tmp.TextAlign = ContentAlignment.MiddleCenter;
+            tmp.Font = new Font("Arial",18);
+            tmp.Width = 45;
+            tmp.Height = 45;
+            tmp.Left = myrand.Next(800,900);
+            tmp.Top = myrand.Next(250,460);
+            tmp.BackColor = Color.FromArgb(1, 202, 207, 210);
+            tmp.BringToFront();
+            brick.Add(tmp);
+            
+            mypanel.Controls.Add(tmp);
+            tmp.BringToFront();
+            
+        }
+        private void initQuestion()
+        {
+            question = new List<string>();
+            //question.Add("practice");
+            //question.Add("hello");
+            //question.Add("good");
+            question.Add("ABC");
+            //question.Add("morning");
+        }
     }
+
 }
